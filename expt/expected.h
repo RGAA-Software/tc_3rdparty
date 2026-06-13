7c705a2eadb7416d758a676afdf649a1bb1cec6f
Kimi <kimi@example.com>
2026-06-13 10:22:53 +0800

    Migrate expt to C++23 std::expected; drop tl::expected

--- expt/expected.h
+++ expt/expected.h
@@ -5,30 +5,31 @@
 #ifndef GAMMARAY_EXPECTED_H
 #define GAMMARAY_EXPECTED_H
 
-#include "expected.hpp"
+#include <expected>
+#include <string>
 
 namespace tc
 {
 
     template<class T, class E>
-    using Result = tl::expected<T, E>;
+    using Result = std::expected<T, E>;
 
     template<typename T>
     using ResultStrErr = Result<T, std::string>;
 
     template<class T>
     static Result<T, std::string> Err(const std::string& err) {
-        return tl::make_unexpected(err);
+        return std::unexpected(err);
     }
 
     template<class T>
     static Result<T, int> ErrInt(int err) {
-        return tl::make_unexpected(err);
+        return std::unexpected(err);
     }
 
 
-#define TRError tl::make_unexpected
-#define TcErr tl::make_unexpected
+#define TRError std::unexpected
+#define TcErr std::unexpected
 
 }
 
